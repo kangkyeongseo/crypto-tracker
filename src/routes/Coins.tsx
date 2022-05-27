7c99ee5ec728cc48447d51c20d84f3dd1f1d2ace
kangkyeongseo <kks_big@naver.com>
2022-05-27 18:22:37 +0900

    Nested Routes

--- src/routes/Coins.tsx
+++ src/routes/Coins.tsx
@@ -23,9 +23,10 @@ const Coin = styled.li`
   margin-bottom: 10px;
   border-radius: 15px;
   a {
+    display: flex;
+    align-items: center;
     transition: color 0.2s ease-in;
     padding: 20px;
-    display: block;
   }
   &:hover {
     a {
@@ -42,6 +43,12 @@ const Title = styled.h1`
 const Loader = styled.span`
   text-align: center;
   display: block;
+`;
+
+const Img = styled.img`
+  width: 35px;
+  height: 35px;
+  margin-right: 10px;
 `;
 
 interface CoinInterface {
@@ -76,7 +83,13 @@ function Coins() {
         <CoinsList>
           {coins.map((coin) => (
             <Coin key={coin.id}>
-              <Link to={`/${coin.id}`}>{coin.name} &rarr;</Link>
+              <Link to={`/${coin.id}`} state={{ name: coin.name }}>
+                <Img
+                  src={`https://cryptocurrencyliveprices.com/img/${coin.id}.png`}
+                  alt={coin.id}
+                />
+                {coin.name} &rarr;
+              </Link>
             </Coin>
           ))}
         </CoinsList>
